e8a765fe391300feea78503e54a61517c95afd67
Caio Augusto <caioaugustobr0106@gmail.com>
2024-01-05 22:19:35 -0300

    first commit

---
+++ src/main/java/com/algaworks/cobrancas/CobrancasSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.cobrancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobrancasSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobrancasSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/cobrancas/CobrancasSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.cobrancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobrancasSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
